83a880b56f5edbf9ad23410bcb425e975c0a7d4a
77ph <33835429+77ph@users.noreply.github.com>
2018-08-21 14:06:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install php7.1 php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-mbstring php7.1-mcrypt php7.1-zip php7.1-fpm
 sudo systemctl restart php7.1-fpm.service
 sudo apt-get install nginx
-sudo apt-get install memcached php-memcached libmemcached
+sudo apt-get install memcached php-memcached 
 
 
 # reserved for the new version
